753a9845ff174e5b733b254495d84f0fc65d136a
Sami Mian <stmian@asu.edu>
2021-12-03 00:27:23 -0700

    Day 3 part 1

---
+++ Day3/main.py
@@ -0,0 +1,33 @@
+with open("input.txt", 'r') as f:
+    dir = [i for i in f.read().splitlines()]
+
+arr = [] 
+
+for n, line in enumerate(dir):
+#    print(line)
+    arr.append(line.split())
+
+ones = [0,0,0,0,0,0,0,0,0,0,0,0]
+zeros = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+for row in arr:
+    for idx, entry in enumerate(row[0]):
+        if entry == "1":
+            ones[idx] += 1
+        elif entry == "0":
+            zeros[idx] += 1
+
+gamma = ""
+epsilon = ""
+
+for i, e in enumerate(ones):
+    if(ones[i] > zeros[i]):
+        gamma += "1"
+        epsilon += "0"
+    else:
+        gamma += "0"
+        epsilon += "1"        
+
+print(int(gamma, 2) *int(epsilon, 2))
+
+
