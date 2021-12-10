0d68d4312d2504f46cf638f8ca07c453abd28fe5
Sami Mian <stmian@asu.edu>
2021-12-09 22:07:37 -0700

    updates

--- Day4/main.py
+++ Day4/main.py
@@ -4,6 +4,7 @@ Created on Sun Dec  5 02:52:19 2021
 
 @author: Sami
 """
+import numpy as np
 
 with open("input.txt", 'r') as f:
     dir = [i for i in f.read().splitlines()]
@@ -15,21 +16,30 @@ blanks = [idx for idx, line in enumerate(dir) if line == ""]
 
 
 sheets={}
+cards = []
 
 for b in blanks:
     sheets["b%s" %b] = dir[b+1:b+6]
     for row in sheets["b%s" %b]:
         row = row.split()
+        row = [int(r.rstrip()) for r in row]
+        cards.append(row)
+
+
+
 
     
   
 for call in inputs[1]:
-    print(call)
-    for s in sheets:
-            if call in sheets[s]:
-                loc = sheets[s].index(call)
-                sheets[s][loc] = "F"
 
-            
+    for card in cards:
+        if int(call) in card:
+            loc = card.index(int(call))
+            card[loc] = -1
 
 
+
+
+def checkBingo():
+    for card in cards:
+        
